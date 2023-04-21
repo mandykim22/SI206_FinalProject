4bf1d7cbedb2540f79bea3407dfd66e06dd40d66
mandykim22 <122408143+mandykim22@users.noreply.github.com>
2023-04-21 14:30:20 -0400

    Add files via upload

--- jp_tool.py
+++ jp_tool.py
@@ -15,33 +15,51 @@ def get_questions_from_category(con, cur, category):
             cur.execute("INSERT INTO questions (id, value, question, category_id) VALUES (?, ?, ?, ?)", (question["id"], question["value"], question["question"], question["category"]["id"]))
     con.commit()
 
-
 def main():
     con = sqlite3.connect("questions.db")
     cur = con.cursor()
 
-    #section 1 begin
+    # section 1 begin
     last_category = 0
     with open("./last_category.txt") as fr:
         last_category = int(fr.readline())
-    
+
     get_questions_from_category(con, cur, last_category)
 
-    with open("/last_category.txt", "w") as fw:
+    with open("./last_category.txt", "w") as fw:
         fw.write(str(last_category+25))
-    #section 1 end
+    # section 1 end
 
-    #section 4 begin
-    top_categories = cur.execute("SELECT c.category, c.question_count, AVG(q.value) FROM questions q INNER JOIN categories c ON q. category_id=c.id GROUP BY q.category_id ORDER BY c.question_count DESC").fetchall()[0:10]
-    #section 4 ends
+    # section 4 begin
+    top_categories = cur.execute("SELECT c.category, c.question_count, AVG(q.value) FROM questions q INNER JOIN categories c ON q.category_id=c.id GROUP BY q.category_id ORDER BY c.question_count DESC").fetchall()[0:10]
+    # section 4 end
 
-    #section 2 begin
+    # section 2 begin
     with open('calculations.csv', 'w', newline='') as csvfile:
         spamwriter = csv.writer(csvfile, delimiter=',')
         spamwriter.writerow(["Category", "Question Count", "Average Value"])
         for top_category in top_categories:
             spamwriter.writerow([top_category[0], top_category[1], top_category[2]])
-    #section 2 end
+    # section 2 end
 
+    # section 3 begin
+    categories = [tup[0] for tup in top_categories]
+    question_counts = [tup[1] for tup in top_categories]
+    avg_points = [tup[2] for tup in top_categories]
+    colors = ["red", "orange", "yellow", "lime", "green", "lightskyblue", "blue", "purple", "grey", "black"]
 
+    plt.barh(categories, question_counts, color=colors)
+    plt.ylabel("Categories")
+    plt.xlabel("# of Questions")
+    plt.title("# of Questions in each Jeaproady Category")
+    plt.show()
 
+    plt.scatter(categories, avg_points, c=colors)
+    plt.ylabel("Categories")
+    plt.xlabel("Average Value of Questions")
+    plt.title("Average Value of Questions in each Jeaproady Category")
+    plt.show()
+    # section 3 end
+
+if __name__ == "__main__":
+    main()
